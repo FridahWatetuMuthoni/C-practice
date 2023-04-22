9ee8ad7d2e529fa47caf091cb81f3f081b8c9f4b
FridahWatetuMuthoni <fridahwatetumuthoni@gmail.com>
2023-04-22 12:48:50 +0300

    print_f basics

---
+++ printf-practise/print.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _printf- a function that produces output according to the format
+ * @format:
+ * Return: the function returns the number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+}
